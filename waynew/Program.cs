e50978ef3e109de1b1ae9e420bed1a99abf0aac8
laimick <kirill.perih.2003@gmail.com>
2022-02-11 11:11:32 +0500

    Добавьте файлы проекта.

---
+++ waynew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace waynew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
